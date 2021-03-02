0f94ab53d80070f287e0ce4842ddfe0b618b917a
Comatoznik <58427696+Comatoznik@users.noreply.github.com>
2021-03-02 18:20:14 +0300

    Added traffic light

---
+++ Light/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace Light
+{
+    public partial class MainPage : ContentPage
+    {
+        StackLayout stackLayout = new StackLayout();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Red_Clicked(object sender, EventArgs e)
+        {
+
+            Background.BackgroundColor = Color.Red;
+        }
+
+        private void Yellow_Clicked(object sender, EventArgs e)
+        {
+            Background.BackgroundColor = Color.Yellow;
+        }
+        private void Green_Clicked(object sender, EventArgs e)
+        {
+            Background.BackgroundColor = Color.Green;
+        }
+    }
+}
