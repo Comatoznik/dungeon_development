830fac43996fb6f69368effcbce10d043a5f746d
Comatoznik <58427696+Comatoznik@users.noreply.github.com>
2021-03-04 17:13:20 +0300

    Add activity work

---
+++ Activity/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Activity
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            MainPage.DisplayAlert("Message", "Onstart", "Continue");
+        }
+
+        protected override void OnSleep()
+        {
+            MainPage.DisplayAlert("Message", "OnSleep", "Continue");
+        }
+
+        protected override void OnResume()
+        {
+            MainPage.DisplayAlert("Message", "OnResume", "Continue");
+        }
+      
+
+    }
+}
