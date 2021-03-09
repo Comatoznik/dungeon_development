d9e1f8a7a806d010bc75694e3f1c15a1a4b45521
Comatoznik <58427696+Comatoznik@users.noreply.github.com>
2021-03-09 21:48:46 +0300

    Add Calculator

---
+++ Calc/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Submit_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = Convert.ToInt32(First.Text);
+                int b = Convert.ToInt32(Second.Text);
+
+                int result = a + b;
+
+                Page1 secondpage = new Page1();
+                Navigation.PushAsync(secondpage);
+                secondpage.Calculator(result);
+            }
+            catch (FormatException)
+            {
+                DisplayAlert("Ошибка", "Ошибка введенных данных!", "Ввести заново");
+                
+            }
+            
+
+        }
+    }
+   
+    }
+
+    
